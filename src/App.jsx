3574a46b9b4289524931913b66d4d455e7a5667d
mintdevil <70203606+mintdevil@users.noreply.github.com>
2024-01-19 18:55:57 +0800

    init vite + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <h1 className="min-h-screen flex items-center justify-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
